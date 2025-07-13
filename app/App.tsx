dd3f267bc13587f623e3ec9e1b7faccdf783e180
KimDog-Studios <kim.dog.543@gmail.com>
2025-07-13 14:26:12 +0100

    Bug Fix 4

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,23 @@
-// src/App.jsx
+// src/components/ProductDetail.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Products from './components/Products';
-import ProductDetail from './components/ProductDetail';
+import { useParams } from 'react-router-dom';
+import products from './config/ProductsConfig';
 
-function App() {
+function ProductDetail() {
+  const { id } = useParams();
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return <h2>Product not found.</h2>;
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      {/* Add more product detail display here */}
+    </div>
   );
 }
 
-export default App;
+export default ProductDetail;
